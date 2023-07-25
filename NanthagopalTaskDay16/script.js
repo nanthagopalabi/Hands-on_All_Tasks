3f0d9f36c7cc1a15d7968c9d5399367262edc3fa
Nanthagopal <welcum2nantha@gmail.com>
2023-07-25 13:16:10 +0530

    TaskDay16

---
+++ NanthagopalTaskDay16/script.js
@@ -0,0 +1,65 @@
+  setTimeout(() =>{
+        const ele1 = document.createElement("count");
+       (ele1.innerText = "10");
+       document.body.append(ele1);
+    
+  setTimeout(() => {
+    const ele2 = document.createElement("count");
+        (ele2.innerText = "9");
+        document.body.append(ele2);
+      
+  setTimeout(() => {
+    const ele3 = document.createElement("count");
+        (ele3.innerText = "8");
+        document.body.append(ele3);
+       
+  setTimeout(() => {
+    const ele4 = document.createElement("count");
+        (ele4.innerText = "7");
+        document.body.append(ele4);
+
+  setTimeout(() => {
+    const ele5 = document.createElement("count");
+        (ele5.innerText = "6");
+        document.body.append(ele5);
+
+  setTimeout(() => {
+    const ele6 = document.createElement("count");
+         (ele6.innerText = "5");
+         document.body.append(ele6);
+
+  setTimeout(() => {
+    const ele7 = document.createElement("count");
+         (ele7.innerText = "4");
+          document.body.append(ele7);
+        
+  setTimeout(() => {
+    const ele8 = document.createElement("count");
+         (ele8.innerText = "3");
+         document.body.append(ele8);
+
+  setTimeout(() => {
+    const ele9 = document.createElement("count");
+        (ele9.innerText = "2");
+         document.body.append(ele9);
+
+  setTimeout(() => {
+    const ele10= document.createElement("count");
+       (ele10.innerText = "1");
+       document.body.append(ele10);
+ 
+  setTimeout(() => {
+    const ele11 = document.createElement("count1");
+        (ele11.innerText = "Happy Independence Day!!!");
+        document.body.append(ele11);
+          }, 0);
+         }, 1000);
+        }, 1000);
+       }, 1000);
+      }, 1000);
+     }, 1000);
+    }, 1000);
+   }, 1000);
+  }, 1000);
+ }, 1000);
+}, 1000);
