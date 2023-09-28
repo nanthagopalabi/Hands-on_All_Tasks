2041494af062c9518bf59c88a19995f157fcae28
Nanthagopal <welcum2nantha@gmail.com>
2023-09-29 01:11:21 +0530

    Task Day-42

--- NanthagopalDayTask42/urlShortnerFrontend/urlShortnerApp/src/ResetNewPassword.jsx
+++ NanthagopalDayTask42/urlShortnerFrontend/urlShortnerApp/src/ResetNewPassword.jsx
@@ -22,7 +22,7 @@ function ResetNewPassword() {
     }
 
     try {
-      const response = await axios.post('https://6515c15bcf81004207cf653a--chic-entremet-8ee05c.netlify.app//confirmation', formData);
+      const response = await axios.post('https://6515c15bcf81004207cf653a--chic-entremet-8ee05c.netlify.apps/confirmation', formData);
       console.log(response.data);
       navigate('/Home');
     } catch (error) {
