f86ffd4e095c0bc25fc1c75057a6f0f624e191a3
Nanthagopal <welcum2nantha@gmail.com>
2023-12-21 23:12:18 +0530

    enhance changes

--- NanthagopalTaskDay16/Callback hell task/script.js
+++ NanthagopalTaskDay16/Callback hell task/script.js
@@ -5,53 +5,58 @@
     
   setTimeout(() => {
     const ele2 = document.createElement("count");
-        (ele2.innerText = "9");
+        (ele1.innerText = "9");
         document.body.append(ele2);
       
   setTimeout(() => {
     const ele3 = document.createElement("count");
-        (ele3.innerText = "8");
+        (ele1.innerText = "8");
         document.body.append(ele3);
        
   setTimeout(() => {
     const ele4 = document.createElement("count");
-        (ele4.innerText = "7");
+        (ele1.innerText = "7");
         document.body.append(ele4);
 
   setTimeout(() => {
     const ele5 = document.createElement("count");
-        (ele5.innerText = "6");
+        (ele1.innerText = "6");
         document.body.append(ele5);
 
   setTimeout(() => {
     const ele6 = document.createElement("count");
-         (ele6.innerText = "5");
+         (ele1.innerText = "5");
          document.body.append(ele6);
 
   setTimeout(() => {
     const ele7 = document.createElement("count");
-         (ele7.innerText = "4");
+         (ele1.innerText = "4");
           document.body.append(ele7);
         
   setTimeout(() => {
     const ele8 = document.createElement("count");
-         (ele8.innerText = "3");
+         (ele1.innerText = "3");
          document.body.append(ele8);
 
   setTimeout(() => {
     const ele9 = document.createElement("count");
-        (ele9.innerText = "2");
+        (ele1.innerText = "2");
          document.body.append(ele9);
 
   setTimeout(() => {
     const ele10= document.createElement("count");
-       (ele10.innerText = "1");
+       (ele1.innerText = "1");
        document.body.append(ele10);
+
+  setTimeout(() => {
+    const ele11= document.createElement("count");
+       (ele1.innerText = " ");
+        document.body.append(ele11);
  
   setTimeout(() => {
-    const ele11 = document.createElement("count1");
-        (ele11.innerText = "Happy Independence Day!!!");
-        document.body.append(ele11);
+    const ele12 = document.createElement("count1");
+        (ele12.innerText = "Happy Independence Day!!!");
+        document.body.append(ele12);
           }, 0);
          }, 1000);
         }, 1000);
@@ -63,3 +68,4 @@
   }, 1000);
  }, 1000);
 }, 1000);
+},1000);
